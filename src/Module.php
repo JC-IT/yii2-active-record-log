09076861376c915f05a48e1479c787c59580dcea
Joey Claessen <joey_claessen@hotmail.com>
2017-07-19 17:08:16 +0000

    wip

--- src/Module.php
+++ src/Module.php
@@ -32,30 +32,34 @@ class Module extends \yii\base\Module
             $classes = $this->logClasses;
         }
 
-        foreach ($classes as $class => $config) {
-            if (is_string($config)) {
-                $class = $config;
-                $config = $this->defaultClassConfig;
+        foreach ($classes as $class => $classConfig) {
+            if (is_string($classConfig)) {
+                $class = $classConfig;
+                $classConfig = $this->defaultClassConfig;
             }
 
-            $readEvents = ArrayHelper::getValue($config, 'readEvents', []);
+            $readEvents = ArrayHelper::getValue($classConfig, 'readEvents', []);
             foreach ($readEvents as $event => $eventConfig) {
                 if (is_string($eventConfig)) {
                    $event = $eventConfig;
                    $eventConfig = [];
                 }
 
+                $eventConfig['logModelClass'] = $eventConfig['logModelClass'] ?? $classConfig['logModelClass'];
+
                 Event::on($class, $event, function(Event $event) use ($eventConfig) {
                     $this->logRead($event->sender, $eventConfig, $event);
                 });
             }
 
-            $updateEvents = ArrayHelper::getValue($config, 'updateEvents', []);
+            $updateEvents = ArrayHelper::getValue($classConfig, 'updateEvents', []);
             foreach ($updateEvents as $event => $eventConfig) {
                 if (is_string($eventConfig)) {
                     $event = $eventConfig;
                     $eventConfig = [];
                 }
+
+                $eventConfig['logModelClass'] = $eventConfig['logModelClass'] ?? $classConfig['logModelClass'];
 
                 Event::on($class, $event, function(Event $event) use ($eventConfig) {
                     $this->logUpdate($event->sender, $eventConfig, $event);
@@ -64,13 +68,43 @@ class Module extends \yii\base\Module
         }
     }
 
-    public function logRead($model, $eventConfig = [], $event = null)
+    public function logRead(ActiveRecord $model, $eventConfig = [], $event = null)
     {
-        vdd($eventConfig);
+        $log = $this->createLog($model, $eventConfig, $event);
+
+        return $this->saveLog($log);
     }
 
-    public function logUpdate($model, $eventConfig = [], $event = null)
+    public function logUpdate(ActiveRecord $model, $eventConfig = [], $event = null)
     {
+        $log = $this->createLog($model, $eventConfig, $event);
+        $log->old_attributes = $model->oldAttributes;
+        return $this->saveLog($log);
+    }
 
+    /**
+     * @param $eventConfig
+     * @return Log
+     */
+    protected function createLog(ActiveRecord $model, $eventConfig = [], $event = null)
+    {
+        $logModelClass = ArrayHelper::getValue($eventConfig, 'modelClass', $this->logModelClass);
+        /** @var Log $log */
+        $log = new $logModelClass();
+        $log->model_class = get_class($model);
+        $log->model_id = $model->getPrimaryKey();
+        $log->event = $event instanceof Event ? $event->name : $event;
+        $log->current_attributes = json_encode($model->attributes);
+        return $log;
+    }
+
+    protected function saveLog(Log $log)
+    {
+        $result = true;
+        //trigger before save to make sure behaviors are triggered (needed when saving in batches)
+        if ($result = $result && $log->beforeSave(true)) {
+            $result = $result && $log->save();
+        }
+        return $result;
     }
 }
--- src/models/Log.php
+++ src/models/Log.php
@@ -2,31 +2,44 @@
 
 namespace JCIT\Yii2\ActiveRecordLog\models;
 
+use yii\behaviors\BlameableBehavior;
+use yii\behaviors\TimestampBehavior;
 use yii\db\ActiveRecord;
+use yii\db\Expression;
 
 /**
  * Class Log
  * @package JCIT\Yii2\ActiveRecordLog\models
  * @property int $id
- * @property string $
+ * @property string $model_class
+ * @property string $model_id
+ * @property string $event
+ * @property string $current_attributes
+ * @property string $old_attributes
+ * @property string $user_id
+ * @property string $created
  */
 class Log extends ActiveRecord
 {
-    protected static $table;
-    protected static $db;
+    public function behaviors()
+    {
+        return [
+            BlameableBehavior::class => [
+                'class' => BlameableBehavior::class,
+                'createdByAttribute' => 'user_id'
+            ],
+            TimestampBehavior::class => [
+                'class' => TimestampBehavior::class,
+                'attributes' => [
+                    ActiveRecord::EVENT_BEFORE_INSERT => ['created']
+                ],
+                'value' => new Expression('NOW()'),
+            ]
+        ];
+    }
 
     public static function tableName()
     {
-        return static::tableName();
-    }
-
-    public static function setDB($db)
-    {
-        static::$db = $db;
-    }
-
-    public static function setTableName($table)
-    {
-        static::$table = $table;
+        return '{{%log}}';
     }
 }
